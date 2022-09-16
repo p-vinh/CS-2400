ff28721722d0703120162c54c65ef666a0c3c4c2
random-debug <85326677+random-debug@users.noreply.github.com>
2022-09-16 10:10:55 -0700

    Revised Add method and Added Security Methods

--- ADT_Bag/Bag.java
+++ ADT_Bag/Bag.java
@@ -1,20 +1,29 @@
 package ADT_Bag;
 
-
 public class Bag<T> implements BagInterface<T> {
 
-    private static int intialCapacity;
-    private T[] bag = (T[]) new Object[intialCapacity];
-    private static int numOfEntries = 0;
+    private static final int DEFAULT_CAPACITY = 25;
+    private final T[] bag;
+    private int numOfEntries;
+    private boolean integrityOK = false;
+    private static final int MAX_CAPACITY = 10000;
 
     public Bag() {
-        this.intialCapacity = 16;
+        this(DEFAULT_CAPACITY);
     }
 
     public Bag(int capacity) {
-        @SuppressWarnings("unchecked")
-        T[] temp = (T[]) new Object[capacity];
-        bag = temp;
+
+        if (capacity <= MAX_CAPACITY) {
+            @SuppressWarnings("unchecked")
+            T[] temp = (T[]) new Object[capacity];
+            bag = temp;
+            numOfEntries = 0;
+            integrityOK = true;
+        }
+        else {
+            throw new IllegalStateException("Attempt to create a bag whose capacity exceeds allowed maximum.");
+        }
     }
 
     public int getCurrentSize() {
@@ -22,22 +31,28 @@ public class Bag<T> implements BagInterface<T> {
     }
 
     public boolean isEmpty() {
-        return bag.length > 0 ? false : true;
+
+        for (int i = 0; i < bag.length; i++) {
+            if (bag[i] != null)
+                return true;
+        }
+        return false;
     }
 
     public boolean add(T newEntry) {
-        try {
-            bag[numOfEntries] = newEntry;
-            System.out.println(bag[numOfEntries]);
-            numOfEntries++;
-            return true;
-        } catch (Exception RuntimeException) {
-            return false;
+        boolean result = true;
+        if(isArrayFull()) {
+            result = false;
         }
+        else {
+            bag[numOfEntries] = newEntry;
+            numOfEntries++;
+        }
+        return result;
     }
 
     public void clear() {
-        for(int i = 0; i < bag.length; i++)
+        for (int i = 0; i < bag.length; i++)
             bag[i] = null;
     }
 
@@ -53,24 +68,47 @@ public class Bag<T> implements BagInterface<T> {
     }
 
     public T remove() {
-        T remove = bag[numOfEntries];
-        bag[numOfEntries] = null;
-        numOfEntries--;
-        return remove;
+        try {
+            System.out.println(!isEmpty());
+            if (!isEmpty()) {
+                numOfEntries--;
+                return bag[numOfEntries];
+            }
+        } catch (Exception RuntimeException) {
+            return null;
+        }
+        return null;
     }
 
     public boolean remove(T anEntry) {
-        
+
         return false;
     }
 
     /**
      * Retrieves all entries that are in this bag.
+     * 
      * @return A newly allocated array of all the entries in the bag.
-     * Note: If the bag is empty, the returned array is empty.
+     *         Note: If the bag is empty, the returned array is empty.
      */
     public T[] toArray() {
+        //The cast is safe because the new array, contains null entries
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) new Object[numOfEntries];
 
-        return null;
+        for (int index = 0; index < result.length; index++) {
+            result[index] = bag[index];
+        }
+        return result;
+    }
+
+    private boolean isArrayFull() {
+        return numOfEntries >= bag.length;
+    }
+
+    private void checkIntegrity() {
+        if (!integrityOK) {
+            throw new SecurityException("Bag Object is corrupt");
+        }
     }
 }
